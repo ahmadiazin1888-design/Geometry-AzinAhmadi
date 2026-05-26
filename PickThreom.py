3fc21fd9c413658eab0203c94490d50a1692f36d
ahmadiazin1888-design <ahmadiazin1888@gmail.com>
2026-05-27 00:19:14 +0330

    PickThreom.py

--- PickThreom.py
+++ PickThreom.py
@@ -19,20 +19,20 @@ st.markdown(
 st.title("محاسبه مساحت چندضلعی های شبکه ای توسط فرمول پيک")
 st.subheader(" آذین احمدی")
 
-st.write("فرمول: S = I + B/2 - 1")
+st.write("فرمول: S = i + b/2 - 1")
 
 # INPUTS
-I = st.number_input("تعداد نقاط داخلی (I)", min_value=0, step=1)
-B = st.number_input("تعداد نقاط مرزی (B)", min_value=0, step=1)
+I = st.number_input("تعداد نقاط داخلی (i)", min_value=0, step=1)
+B = st.number_input("تعداد نقاط مرزی (b)", min_value=0, step=1)
 
 # CALC
 if st.button("محاسبه مساحت"):
-    S = I + (B / 2) - 1
+    S = i + (b / 2) - 1
 
     st.success(f"مساحت چندضلعی: {S}")
 
     st.markdown("### توضیح")
-    st.write("طبق قضیه پیک، مساحت از ترکیب نقاط داخلی و مرزی محاسبه می‌شود.")
+    st.write("طبق قضیه پیک، مساحت از ترکیب نقاط داخلی و مرزی و فرمول S = i + b/2 - 1 محاسبه می‌شود.")
 
 st.markdown("---")
 st.caption("Azin Ahmadi - 102")
